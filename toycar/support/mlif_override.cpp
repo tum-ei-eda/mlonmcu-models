9d649de656f29d8e83a0d8805cc01c003774f3c5
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-09-25 07:09:17 +0200

    refactor exit code handling for validation feature

--- toycar/support/mlif_override.cpp
+++ toycar/support/mlif_override.cpp
@@ -4,14 +4,14 @@
 
 
 extern "C" {
-void mlif_process_input(const void *, size_t, void *, size_t);
-void mlif_process_output(void *, size_t, const void *, size_t);
+int mlif_process_input(const void *, size_t, void *, size_t);
+int mlif_process_output(void *, size_t, const void *, size_t);
 }
 
 const float *current_input = 0;
 
 
-void mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz)
+int mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz)
 {
     float in_scale = 0.39101523160934448;
     int in_zeropt = 89;
@@ -19,7 +19,7 @@ void mlif_process_input(const void *in_data, size_t in_size, void *model_input_p
     if (in_size / 4 != model_input_sz)
     {
         DBGPRINTF("MLIF: Wrong input size!\n");
-        exit(EXIT_MLIF_INVALID_SIZE);
+        return EXIT_MLIF_INVALID_SIZE;
     }
 
     for (int i = 0; i < model_input_sz; i++)
@@ -28,14 +28,15 @@ void mlif_process_input(const void *in_data, size_t in_size, void *model_input_p
     }
 
     current_input = (const float*)in_data;
+    return 0;
 }
 
-void mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
+int mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
 {
     if (model_output_sz != 640)
     {
         DBGPRINTF("MLIF: Wrong output size!\n");
-        exit(EXIT_MLIF_INVALID_SIZE);
+        return EXIT_MLIF_INVALID_SIZE;
     }
 
     float out_scale = 0.36449846625328064;
@@ -56,6 +57,7 @@ void mlif_process_output(void *model_output_ptr, size_t model_output_sz, const v
     if (diffsum != expected)
     {
         DBGPRINTF("MLIF: Wrong output! Expected %f\n", expected);
-        exit(EXIT_MLIF_MISSMATCH);
+        return EXIT_MLIF_MISSMATCH;
     }
+    return 0;
 }
