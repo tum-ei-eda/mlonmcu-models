eb2bd0459518dc2b2cc45514986a151946ab8191
Philipp van Kempen <philipp.van-kempen@tum.de>
2023-09-25 07:09:17 +0200

    refactor exit code handling for validation feature

--- person_detect/support/mlif_override.cpp
+++ person_detect/support/mlif_override.cpp
@@ -5,29 +5,30 @@
 
 
 extern "C" {
-void mlif_process_input(const void *, size_t, void *, size_t);
-void mlif_process_output(void *, size_t, const void *, size_t);
+int mlif_process_input(const void *, size_t, void *, size_t);
+int mlif_process_output(void *, size_t, const void *, size_t);
 }
 
 
-void mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz)
+int mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz)
 {
     if (in_size != model_input_sz)
     {
         DBGPRINTF("MLIF: Wrong input size!\n");
-        exit(EXIT_MLIF_INVALID_SIZE);
+        return EXIT_MLIF_INVALID_SIZE;
     }
 
     memcpy(model_input_ptr, in_data, in_size);
+    return 0;
 }
 
-void mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
+int mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
 {
     const char* labels[] = {"person", "no_person"};
     if (model_output_sz != 2)
     {
         DBGPRINTF("MLIF: Wrong output size!\n");
-        exit(EXIT_MLIF_INVALID_SIZE);
+        return EXIT_MLIF_INVALID_SIZE;
     }
 
     float out_scale = 0.00390625;
@@ -54,7 +55,8 @@ void mlif_process_output(void *model_output_ptr, size_t model_output_sz, const v
         if (results[i] != expected)
         {
             DBGPRINTF("MLIF: Wrong output in category %i! Expected %.9g\n", i, expected);
-            exit(EXIT_MLIF_MISSMATCH);
+            return EXIT_MLIF_MISSMATCH;
         }
     }
+    return 0;
 }
--- vww/support/mlif_override.cpp
+++ vww/support/mlif_override.cpp
@@ -5,12 +5,12 @@
 
 
 extern "C" {
-void mlif_process_input(const void *, size_t, void *, size_t);
-void mlif_process_output(void *, size_t, const void *, size_t);
+int mlif_process_input(const void *, size_t, void *, size_t);
+int mlif_process_output(void *, size_t, const void *, size_t);
 }
 
 
-void mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz)
+int mlif_process_input(const void *in_data, size_t in_size, void *model_input_ptr, size_t model_input_sz)
 {
     float in_scale = 0.0039215688593685627;
     int in_zeropt = -128;
@@ -18,21 +18,22 @@ void mlif_process_input(const void *in_data, size_t in_size, void *model_input_p
     if (in_size / 4 != model_input_sz)
     {
         DBGPRINTF("MLIF: Wrong input size!\n");
-        exit(EXIT_MLIF_INVALID_SIZE);
+        return EXIT_MLIF_INVALID_SIZE;
     }
 
     for (int i = 0; i < model_input_sz; i++)
     {
         ((int8_t*)model_input_ptr)[i] = QuantizeFloatToInt8(((float*)in_data)[i], in_scale, in_zeropt) - 128;
     }
+    return 0;
 }
 
-void mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
+int mlif_process_output(void *model_output_ptr, size_t model_output_sz, const void *expected_out_data, size_t expected_out_size)
 {
     if (model_output_sz != 2)
     {
         DBGPRINTF("MLIF: Wrong output size!\n");
-        exit(EXIT_MLIF_INVALID_SIZE);
+        return EXIT_MLIF_INVALID_SIZE;
     }
 
     float out_scale = 0.00390625;
@@ -59,7 +60,8 @@ void mlif_process_output(void *model_output_ptr, size_t model_output_sz, const v
         if (results[i] != expected)
         {
             DBGPRINTF("MLIF: Wrong output in category %i! Expected %.9g\n", i, expected);
-            exit(EXIT_MLIF_MISSMATCH);
+            return EXIT_MLIF_MISSMATCH;
         }
     }
+    return 0;
 }
