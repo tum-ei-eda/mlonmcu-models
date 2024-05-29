e5cc103dae27b440e7accd84fda51ae343a04dc6
Philipp van Kempen <philipp.van-kempen@tum.de>
2024-05-29 10:45:05 +0200

    update download_data.sh scripts

--- aww/download_data.sh
+++ aww/download_data.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Inputs
 wget https://syncandshare.lrz.de/dl/fiCKCAjLe8xC99wB1JXNT/validation_data/aww_input.tar.xz
 cd input
 tar xf ../aww_input.tar.xz
 cd -
 rm aww_input.tar.xz
+
+# Outputs (TFLite)
+wget https://syncandshare.lrz.de/dl/fiCKCAjLe8xC99wB1JXNT/validation_data/aww_output_tflite.tar.xz
+cd output_tflite
+tar xf ../aww_output_tflite.tar.xz
+cd -
+rm aww_output_tflite.tar.xz
--- resnet/download_data.sh
+++ resnet/download_data.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Inputs
 wget https://syncandshare.lrz.de/dl/fiCKCAjLe8xC99wB1JXNT/validation_data/resnet_input.tar.xz
 cd input
 tar xf ../resnet_input.tar.xz
 cd -
 rm resnet_input.tar.xz
+
+# Outputs (TFLite)
+wget https://syncandshare.lrz.de/dl/fiCKCAjLe8xC99wB1JXNT/validation_data/resnet_output_tflite.tar.xz
+cd output_tflite
+tar xf ../resnet_output_tflite.tar.xz
+cd -
+rm resnet_output_tflite.tar.xz
